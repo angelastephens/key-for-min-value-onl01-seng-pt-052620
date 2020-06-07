6e4def3faec88d309fa8fc69c013fae880d5a193
Angela Stephens <angelastephens317@gmail.com>
2020-06-07 01:18:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  empty_hash=nil
   name_hash.collect do |key,value|
     if value <= 1
       return name
     end
   end
-  name_hash
+  empty_hash
 end
