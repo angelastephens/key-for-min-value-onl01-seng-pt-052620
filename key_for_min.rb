38ff2be0c84157839ee778534c889ba52d111d64
Angela Stephens <angelastephens317@gmail.com>
2020-06-07 01:27:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   name_hash.collect do |name,value|
     if value <= 1
       return name
-    elsif value <=10
+    elsif name < value
       return name
     end
   end
